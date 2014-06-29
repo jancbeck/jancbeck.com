4b49ef2b0a8658e2f64ee55e3decf2edfdc4e5d8
Jan Beck <delite.jcb@googlemail.com>
2014-06-29 21:10:36 +0200

    removed piwik tracking script

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -17,22 +17,5 @@
 
 <a class="skip" href="#top">To top</a>
 
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['setDoNotTrack', true]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    _paq.push(['setTrackerUrl', '//analytics.jancbeck.com/'+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-    g.defer=true; g.async=true; g.src='/piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-
-</script>
-<noscript><p><img src="//analytics.jancbeck.com/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Code -->
-
 </body>
 </html>
