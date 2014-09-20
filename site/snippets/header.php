efc3ec4fa2473bffcbe1b3433b9399840c887060
Jan Beck <delite.jcb@googlemail.com>
2014-09-20 15:15:06 +0200

    fix path to ios icon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -64,7 +64,7 @@ if ( $page->description() ) {
 
 <!-- Favicons -->
 <link rel="shortcut icon" href="<?php echo url('assets/images/icons/favicon.ico') ?>" />
-<link rel="apple-touch-icon" href="<?php echo url('assets/images/icons/icons/iOS/apple-touch-icon-72x72.png') ?>" />
+<link rel="apple-touch-icon" href="<?php echo url('assets/images/icons/iOS/apple-touch-icon-72x72.png') ?>" />
 <link rel="apple-touch-icon" sizes="72x72" href="<?php echo url('assets/images/icons/iOS/apple-touch-icon-72x72.png') ?>" />
 <link rel="apple-touch-icon" sizes="114x114" href="<?php echo url('assets/images/icons/iOS/apple-touch-icon-114x114.png') ?>" />
 
