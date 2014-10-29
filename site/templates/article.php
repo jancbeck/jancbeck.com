4f2f7b5e6c4ffe82b0f4daabcc5ae5839cb84986
Jan Beck <delite.jcb@googlemail.com>
2014-10-29 18:30:59 +0100

    updated output of article dates

--- site/templates/article.php
+++ site/templates/article.php
@@ -7,8 +7,8 @@
 		<?php echo kirbytext($page->text()); ?>
 		<p>
 			<em>Written by <?php echo html($site->googleauthor()) ?> on 
-			<time datetime="<?php echo date( DATE_ATOM, strtotime( $page->published()) ) ?>">
-				<?php echo html(date( "F j, Y", strtotime( $page->published()))) ?>
+			<time datetime="<?php echo $page->date('c') ?>" pubdate="pubdate">
+				<?php echo $page->date('F j, Y') ?>
 			</time> 
 			in <?php echo html($page->location())?>.</em>
 		</p>
