139d67d6c4dc6146c8331ba85520776f53fa6f20
Jan Beck <delite.jcb@googlemail.com>
2014-10-29 18:52:40 +0100

    use 'url' function to display feed link

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,7 +5,7 @@
       <li><a href="http://twitter.com/jancbeck/"><i class="icon-twitter"></i> <span>Twitter</span></a></li>
       <li><a href="http://github.com/jancbeck/"><i class="icon-github"></i> <span>Github</span></a></li>
       <li><a href="mailto:mail@jancbeck.com"><i class="icon-mail"></i> <span>E-Mail</span></a></li>
-      <li><a href="http://jancbeck.com/feed/"><i class="icon-feed"></i> <span>Feed</span></a></li>
+      <li><a href="<?php echo url('feed') ?>"><i class="icon-feed"></i> <span>Feed</span></a></li>
     </ul>
   </nav>
 
