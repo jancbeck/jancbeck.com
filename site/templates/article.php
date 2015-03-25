f4a80b10d68b5e26afaab1d9c9c05a037f443495
Jan Beck <delite.jcb@googlemail.com>
2015-03-25 12:59:28 -0500

    rearranged article byline

--- site/templates/article.php
+++ site/templates/article.php
@@ -5,14 +5,14 @@
 	<!--Article-->
 	<article role="article">
 		<h2><?php echo kirbytext($page->title()); ?></h2>
-		<?php echo kirbytext($page->text()); ?>
-		<p>
-			<em>Written by <?php echo html($site->googleauthor()) ?> on 
-			<time datetime="<?php echo $page->date('c') ?>" pubdate="pubdate">
+		<h6>
+			Written by <b><?php echo html($site->googleauthor()) ?></b> on 
+			<b><time datetime="<?php echo $page->date('c') ?>" pubdate="pubdate">
 				<?php echo $page->date('F j, Y') ?>
-			</time> 
-			in <?php echo html($page->location())?>.</em>
-		</p>
+			</time> </b>
+			in <b><?php echo html($page->location())?></b>.
+		</h6>
+		<?php echo kirbytext($page->text()); ?>
 	</article>
 
 </section>
