abb39558a844a7b1d56708baa74f01e93ae97b2b
Jan Beck <delite.jcb@googlemail.com>
2015-04-28 12:19:22 -0500

    added a non-working implementation of src-set attribute. will fix later.

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -1,9 +1,11 @@
 <section id="projects">
 	<h2><?php echo $data->title()->html() ?></h2>
 
-	<?php foreach($data->children()->visible() as $project): ?>
+	<?php foreach($data->children()->visible() as $project): 
+		$thumb = thumb( $project->images()->first(), array('width' => 500, 'grayscale' => true));
+	?>
 		<figure>
-			<a href="<?php echo $project->link()->html() ?>"><img src="<?php echo $project->images()->first()->url() ?>" alt="<?php echo $project->title()->html() ?>"></a>
+			<a href="<?php echo $project->link()->html() ?>"><img src="<?php echo $project->images()->first()->url() ?>" srcset="<?php echo $thumb->url() ?> <?php echo $thumb->width() ?>w, <?php echo $project->images()->first()->url() ?> <?php echo $project->images()->first()->width() ?>w" alt="<?php echo $project->title()->html() ?>"></a>
 		</figure>
 		<h3><?php echo $project->title()->html() ?></h3>
 		<p><?php echo $project->text()->html() ?></p>
