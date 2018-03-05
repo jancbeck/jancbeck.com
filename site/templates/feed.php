dd46e5fcb0e6913be5671a919eb43ff8960da35b
Jan Beck <jancbeck@users.noreply.github.com>
2018-03-05 09:56:33 +0100

    fixing feed

--- site/templates/feed.php
+++ site/templates/feed.php
@@ -4,7 +4,7 @@ echo page('articles')->children()->visible()->flip()->limit(10)->feed(array(
   'title'       => $page->title(),
   'description' => $page->text(),
   'link'        => 'articles',
-  'snippet'     => 'feed'
+  //'snippet'     => 'feed'
 ));
 
 ?>
