8a7d6d5cb1604b9524ac721a8f7294e952e89d5d
Jan Beck <jancbeck@users.noreply.github.com>
2018-05-24 19:48:19 +0200

    GDPR ❤️

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,25 +1,8 @@
 <footer>
 	<section>
-		<p><small>Powered by <a href="http://getkirby.com/">kirby</a> • Hosted by <a href="//uberspace.de/">uberspace</a></small></p>
+		<p><small>Powered by <a href="https://getkirby.com/">kirby</a> • Hosted by <a href="https://uberspace.de/">uberspace</a></small></p>
 	</section>
 </footer>
-
-<!-- Piwik -->
-<script type="text/javascript">
-	var _paq = _paq || [];
-	_paq.push(['trackPageView']);
-	_paq.push(['enableLinkTracking']);
-	(function() {
-		var u=(("https:" == document.location.protocol) ? "https" : "http") + "://<?php echo $site->piwik_url() ?>/";
-		_paq.push(['setTrackerUrl', u+'piwik.php']);
-		_paq.push(['setSiteId', 1]);
-		var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
-		g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-	})();
-</script>
-<noscript><p><img src="//<?php echo $site->piwik_url() ?>/piwik.php?idsite=<?php echo $site->piwik_id() ?>" style="border:0;" alt="Piwik Tracking Pixel" /></p></noscript>
-<!-- End Piwik Code -->
-
 
 </body>
 </html>
